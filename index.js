adf8fb93fadc819a055a56c58280459d02c62ae0
Andy <amedamachip713@gmail.com>
2017-08-12 19:51:26 -0500

    homework

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var _ = require('lodash');                                   //L1
 var wine = 'I like red wine';                                //L2
 var h4Heading = document.querySelector('h4');                //L3
 h4Heading.textContent = _.replace(wine, 'red', 'white');     //L4
+
+var $ = require('jquery');
+$('h4').css('color', 'white');
